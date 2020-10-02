afca49aa3a1437a1e1c41c9319b376fefe210f12
jglunac <59965122+jglunac@users.noreply.github.com>
2020-10-01 20:55:44 -0600

    Método hallar repeticiones

---
+++ HuffmanCompression/TestConsole/Letter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    class Letter
+    {
+        public int Frecuency { get; set; }
+        public string Key { get; set; }
+    }
+}
